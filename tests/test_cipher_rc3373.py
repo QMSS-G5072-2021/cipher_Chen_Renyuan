b4a16baf35a69712da5781ad28097a122b0372d8
陈人源 <alicechen@macbook-air.myfiosgateway.com>
2021-11-10 17:00:28 -0500

    Add test function

--- tests/test_cipher_rc3373.py
+++ tests/test_cipher_rc3373.py
@@ -1,1 +1,7 @@
 from cipher_rc3373 import cipher_rc3373
+import pytest
+def test_cipher_with_word():
+    example = ['Love',1]
+    expected = 'Mpwf'
+    actual = cipher_rc3373.cipher(example[0], example[1], encrypt=True)
+    assert actual == expected, "Don't work with a single word"
